4801e8cad8ac42168d1d1a2a2c07902aed1fbc24
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-10 17:14:55 +0530

    Feat : Auth setup in appwrite

--- BlogWebapp/src/appwrite/auth.js
+++ BlogWebapp/src/appwrite/auth.js
@@ -1,10 +1,64 @@
 import conf from "../components/conf/conf";
+import { Client, Account, ID } from "appwrite";
 
 export class AuthServices {
-    
+  client = new Client();
+  account;
 
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log(
+        `Appwrite service error :: auth :: createAccount :: ${error}`
+      );
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log(`Appwrite service error :: auth :: login :: ${error}`);
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log(`Appwrite service error :: auth :: logout :: ${error}`);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(
+        `Appwrite service error :: auth :: getCurrentUser :: ${error}`
+      );
+    }
+  }
 }
 
 const authService = AuthServices;
 
-export default AuthServices;
+export default authService;

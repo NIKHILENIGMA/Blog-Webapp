17252fb8d461d19adcb9a65e043ba32b9f7ba518
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-11 16:28:20 +0530

    Feat: header and footer component

--- BlogWebapp/src/components/Footer/Footer.jsx
+++ BlogWebapp/src/components/Footer/Footer.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+// import Logo from "../Logo";
 
-export default function Footer() {
-    return (
-<footer>Footer</footer>
-    );
+function Footer() {
+  return (
+    <>
+      <div className=" bg-black w-full h-[40vh] text-white shadow-md flex">
+        <div className=" bg-red-400 w-[25%] h-full ">
+          <div className="bg-green-700 px-20 h-full">
+            <h1 className="uppercase py-5 text-start ">Company Name</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Doloribus quo iusto in adipisci! Mollitia nulla dolorum id
+              nesciunt consectetur? Nostrum exercitationem tenetur vel assumenda
+              quasi, omnis dolor dolorum ullam blanditiis!
+            </p>
+          </div>
+        </div>
+        <div className="bg-blue-400 w-[25%] h-full">
+          <div className="bg-blue-800 px-20 h-full w-[80%] ml-10">
+            <h1 className="uppercase py-5 text-start ">PRODUCTS</h1>
+            <Link>
+
+            </Link>
+          </div>
+        </div>
+        <div className="bg-green-400 w-[25%] h-full"></div>
+        <div className="bg-indigo-400 w-[25%] h-full"></div>
+      </div>
+    </>
+  );
 }
+
+export default Footer;
--- BlogWebapp/src/components/Header/Header.jsx
+++ BlogWebapp/src/components/Header/Header.jsx
@@ -1,5 +1,64 @@
 import React from "react";
+import { useSelector } from "react-redux";
+// import { useNavigate, Link } from "react-router-dom";
+import { LogoutBtn, Logo } from "../index";
 
 export default function Header() {
-  return <header>Header</header>;
+  const autheStatus = useSelector((state) => state.auth.status);
+  // const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "About",
+      slug: "/about",
+      active: true,
+    },
+    {
+      name: "Blog",
+      slug: "/blog",
+      active: true,
+    },
+    {
+      name: "SignUp",
+      slug: "/signup",
+      active: !autheStatus,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !autheStatus,
+    },
+  ];
+
+  return (
+    <header className=" py-3 shadow-md bg-gray-300 ">
+      <nav className="flex">
+        //Todo add link to logo
+        <div className="mr-4">Logo</div>
+        <ul className=" flex ml-auto">
+          {navItems.map((item) =>
+            item.active ? (
+              <li key={item.name}>
+                <button
+                // onClick={} //Todo use slug navigate
+                >
+                  {item.name}
+                </button>
+              </li>
+            ) : null
+          )}
+          {autheStatus && (
+            <li>
+              <LogoutBtn />
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }

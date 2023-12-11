fa4c57ca38ecf8d3208bb818d8cfc8599e73e61e
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-11 13:42:05 +0530

    feat : setup auth method

--- BlogWebapp/src/App.jsx
+++ BlogWebapp/src/App.jsx
@@ -1,12 +1,41 @@
-import Header from "./components/Header/Header";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/auth/authSlice";
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "./components";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h2> Hello World</h2>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  // {console.log(authService.getCurrentUser);}
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) => {
+        console.error(`Error occur :: App :: getCurrentUser :: ${err} `);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+           <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;

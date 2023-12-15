83d1953f85e1dda554d98aca0ad4a870eb87b2b2
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-15 10:39:13 +0530

    Feat : Logo css fixed

--- BlogWebapp/src/components/Logo.jsx
+++ BlogWebapp/src/components/Logo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Logo() {
   return (
-    <div>
-      <img src="$" alt="Logo" />
+    <div className="w-[90px] ">
+      <img src="https://png.pngtree.com/png-clipart/20220117/original/pngtree-cool-red-wings-logo-png-image_7129340.png" alt="Logo" />
     </div>
   );
 }

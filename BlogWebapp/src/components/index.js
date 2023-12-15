055839b899d64731f1140f5df3a7007e38529ef9
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-15 10:41:08 +0530

    Chore : component path set

--- BlogWebapp/src/components/index.js
+++ BlogWebapp/src/components/index.js
@@ -1,5 +1,27 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
 import Logo from "./Logo";
-export { Header, Footer, LogoutBtn, Logo };
+import Select from "./Select";
+import Input from "./Input";
+import Card from "./Card";
+import Layout from "./Layout";
+import Signup from "./Signup";
+import HomeCards from "./HomeCards";
+
+export {
+  Header,
+  Footer,
+  LogoutBtn,
+  Logo,
+  Button,
+  Select,
+  Input,
+  Card,
+  Login,
+  Layout,
+  Signup,
+  HomeCards
+};

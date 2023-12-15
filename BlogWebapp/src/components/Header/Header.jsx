f50c50e90be07732f86b40a6d20bc34d12c697d9
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-15 10:33:15 +0530

    Feat : Header set navigation

--- BlogWebapp/src/components/Header/Header.jsx
+++ BlogWebapp/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-// import { useNavigate, Link } from "react-router-dom";
-import { LogoutBtn, Logo } from "../index";
+import { useNavigate, Link } from "react-router-dom";
+import { LogoutBtn, Logo, Button } from "../index";
+
 
 export default function Header() {
   const autheStatus = useSelector((state) => state.auth.status);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const navItems = [
     {
@@ -24,8 +25,8 @@ export default function Header() {
       active: true,
     },
     {
-      name: "SignUp",
-      slug: "/signup",
+      name: "Contact",
+      slug: "/contact",
       active: !autheStatus,
     },
     {
@@ -33,22 +34,32 @@ export default function Header() {
       slug: "/login",
       active: !autheStatus,
     },
+    // {
+    //   name: "Sign Up",
+    //   slug: "/signup",
+    //   active: !autheStatus,
+    // },
   ];
 
   return (
-    <header className=" py-3 shadow-md bg-gray-300 ">
+    <header className="sticky top-0 z-50 mx-32 rounded-lg py-3 shadow-lg bg-white ">
       <nav className="flex">
-        //Todo add link to logo
-        <div className="mr-4">Logo</div>
-        <ul className=" flex ml-auto">
+        {/* //Todo add link to logo */}
+        <div className="mr-4 mx-2">
+          <Link to={"/"}>
+            <Logo />
+          </Link>
+        </div>
+        <ul className=" w-[70%] flex justify-evenly items-center  ">
           {navItems.map((item) =>
             item.active ? (
               <li key={item.name}>
-                <button
-                // onClick={} //Todo use slug navigate
+                <Button
+                  onClick={()=>navigate(item.slug)}
+                  className="bg-transparent"
                 >
                   {item.name}
-                </button>
+                </Button>
               </li>
             ) : null
           )}
@@ -62,3 +73,5 @@ export default function Header() {
     </header>
   );
 }
+
+// npm i @tinymce/tinymce-react

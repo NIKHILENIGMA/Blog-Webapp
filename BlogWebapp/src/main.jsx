42c7dc2ad214dd30a8fa5399d4ce1d2e737e5832
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-15 10:41:41 +0530

    Bug : Fixed navigation issues set new router

--- BlogWebapp/src/main.jsx
+++ BlogWebapp/src/main.jsx
@@ -4,11 +4,60 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import { Layout, Login } from "./components/index.js";
+import Signup from "./pages/Signup";
+import Post from "./pages/Post";
+import AllPosts from "./pages/AllPosts.jsx";
+import About from "./pages/About.jsx";
+import Blog from "./pages/Blog.jsx";
+import Contact from "./pages/Contact.jsx";
+// import Edit from "./pages/Edit.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Layout authentication={false}>
+            {" "}
+            <Login />
+          </Layout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );

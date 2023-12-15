1497f8854454762604f9837c530def319f20f8f9
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-15 10:42:11 +0530

    Feat : Create a about page

---
+++ BlogWebapp/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
+        quisquam in cumque enim ipsam eaque voluptatum, quo assumenda temporibus
+        autem dolores earum officia itaque repellendus tempore facere velit,
+        amet dolor!
+      </div>
+      
+    </>
+  );
+}
+
+export default About;
